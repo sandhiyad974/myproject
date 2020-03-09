113b7dd977e6844511e2250c9761344ca26757af
sandhiyad974 <58321669+sandhiyad974@users.noreply.github.com>
2020-03-09 21:24:11 +0530

    Add files via upload

---
+++ difference.java
@@ -0,0 +1,21 @@
+package newteAM;
+import java.util.Scanner;
+class Difference {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.print("No.Of.Elements : ");
+		int number=input.nextInt();
+		System.out.print("Start : ");
+		int start=input.nextInt();
+		System.out.print("Difference : ");
+		int total_Numbers=input.nextInt();
+			System.out.print(start);
+				for(int i=1;i<number;i++)
+				{
+					System.out.print(",");
+					System.out.print(start+(total_Numbers*i));
+				}
+		}
+
+}
