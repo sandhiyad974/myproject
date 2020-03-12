681cf6f08bda327dd0fac14d8281f756b139f2d9
sandhiyad974 <58321669+sandhiyad974@users.noreply.github.com>
2020-03-12 19:05:28 +0530

    Add files via upload

---
+++ automprphic_Number.java
@@ -0,0 +1,25 @@
+package newteAM;
+import java.util.Scanner;
+class Automorphic_Number {
+	public static void main(String args[]) {
+		System.out.print("Enter any number :");
+		Scanner sn=new Scanner(System.in);
+		int number=sn.nextInt();
+		int temp=10;
+		if (temp>=number) {
+			if ((number*number)%temp==number) {
+				System.out.println("automarphic number");
+			}
+			else {
+				System.out.println("not automarphic number");
+			}
+		} 
+		else if (temp<=number) {
+			if ((number*number)%(temp*10)==number) {
+				System.out.println("automarphic number");
+			} else {
+				System.out.println("not automarphic number");
+			}
+		}
+	}
+}
