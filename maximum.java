adddf719dfe8ecd566703674b4fc32919be62b75
sandhiyad974 <58321669+sandhiyad974@users.noreply.github.com>
2020-03-17 10:11:14 +0530

    Add files via upload

---
+++ maximum.java
@@ -0,0 +1,25 @@
+package newteAM;
+import java.util.Scanner;
+class GetMaximum {
+	   public static void main(String[] args){ 
+	        Scanner input=new Scanner(System.in);
+	        System.out.print("Enter the number : ");
+	        int num=input.nextInt();
+	        int[] arr = new int[10];
+	        while(true){
+	            if(num==0){
+	                break;
+	            }
+	            else {
+	            	int val = num%10;
+	        		arr[val]++;
+	        	    num /= 10;
+	            }
+	        }
+	        for(int i=9; i>=0; i--){
+	            for(int j=0; j<arr[i]; j++){
+	                System.out.print("The highest number : "+i);
+	            }
+	        }
+	   }
+  } 
