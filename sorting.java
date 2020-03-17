9d57c06ec0375bab84c82506573ccf1178a24cd0
sandhiyad974 <58321669+sandhiyad974@users.noreply.github.com>
2020-03-17 10:26:05 +0530

    Add files via upload

---
+++ sorting.java
@@ -0,0 +1,33 @@
+package newteAM;
+import java.util.Scanner;
+class Sorting {
+	public static void main(String[] args) {
+		int noOfEle, i, j, temp;  
+		int arr[] = new int[20];  
+		Scanner scan = new Scanner(System.in);  		         
+		System.out.print("Enter number of elements in array < 20 : ");  
+		noOfEle = scan.nextInt();  
+		System.out.print("Enter Array Elements : ");  
+		for(i=0; i<noOfEle; i++)  
+		{  
+			arr[i] = scan.nextInt();  
+		}  
+		for(i=0; i<noOfEle; i++)  
+		{  
+			for(j=i+1; j<noOfEle; j++)  
+		    {  
+				if(arr[i] > arr[j])  
+		        {  
+					temp = arr[i];  
+		            arr[i] = arr[j];  
+		            arr[j] = temp;  
+		        }  
+		    }  
+		}  
+		
+			for(i=0; i<noOfEle; i++)  
+		    {  
+				System.out.print(arr[i]+ "  ");  
+		    }  
+	}  
+}  
